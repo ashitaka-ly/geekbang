f4e5579b61766c50faa7fcf53d15037efd7c686b
Ashitaka-ly <liuy_61@foxmail.com>
2022-07-23 20:04:33 +0800

    httpserver 2

--- httpserver/main.go
+++ httpserver/main.go
@@ -5,32 +5,44 @@ import (
 	"fmt"
 	"github.com/golang/glog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	flag.Set("v", "4")
 	glog.V(2).Info("starting")
 	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/header", headerHandler)
+	http.HandleFunc("/version", versionHandler)
 	http.HandleFunc("/healthz", healthzHandler)
-	//http.Handle("/healthz", http.HandleFunc(http.TimeoutHandler(healthzHandler, time.Second * 5, "")))
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		glog.V(2).Info(err)
 	}
 }
 
-func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "welcome")
+func headerHandler(w http.ResponseWriter, req *http.Request) {
+	header := req.Header
+	for key, values := range header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "getHeader")
+}
+
+func versionHandler(w http.ResponseWriter, req *http.Request) {
+	version := os.Getenv("VERSION")
+	w.Header().Add("VERSION", version)
+	fmt.Fprintf(w, "getVersion")
+}
+
+func welcomeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "welcome")
 }
 
 func healthzHandler(w http.ResponseWriter, req *http.Request) {
-	header := req.Header
-	for key, values := range header {
-		for index, value := range values {
-			w.Header().Add(key, value)
-			fmt.Println(index)
-		}
-	}
 	w.WriteHeader(200)
 	fmt.Fprintf(w, "200")
 }
