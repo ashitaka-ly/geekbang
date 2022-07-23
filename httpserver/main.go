177f9c66db259a84da3a093b2f5d10684f42a196
Ashitaka-ly <liuy_61@foxmail.com>
2022-07-23 19:52:32 +0800

    httpserver

--- httpserver/main.go
+++ httpserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/golang/glog"
 	"net/http"
 )
@@ -9,10 +10,27 @@ import (
 func main() {
 	flag.Set("v", "4")
 	glog.V(2).Info("starting")
-	http.HandleFunc("/", rootHandler)
-
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/healthz", healthzHandler)
+	//http.Handle("/healthz", http.HandleFunc(http.TimeoutHandler(healthzHandler, time.Second * 5, "")))
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		glog.V(2).Info(err)
+	}
 }
 
-func rootHandler(writer http.ResponseWriter, request *http.Request) {
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "welcome")
+}
 
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	header := req.Header
+	for key, values := range header {
+		for index, value := range values {
+			w.Header().Add(key, value)
+			fmt.Println(index)
+		}
+	}
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "200")
 }
