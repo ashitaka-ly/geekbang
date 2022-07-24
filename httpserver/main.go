c80e0cbf873eeac409376221fe0205d6572677d1
Ashitaka-ly <liuy_61@foxmail.com>
2022-07-24 15:09:45 +0800

    httpserver The final version

--- httpserver/main.go
+++ httpserver/main.go
@@ -4,45 +4,77 @@ import (
 	"flag"
 	"fmt"
 	"github.com/golang/glog"
+	"log"
+	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
+	"strings"
 )
 
 func main() {
 	flag.Set("v", "4")
-	glog.V(2).Info("starting")
-	http.HandleFunc("/", welcomeHandler)
-	http.HandleFunc("/header", headerHandler)
-	http.HandleFunc("/version", versionHandler)
-	http.HandleFunc("/healthz", healthzHandler)
-	err := http.ListenAndServe(":80", nil)
+	glog.V(2).Info("program starting")
+
+	mux := http.NewServeMux()
+	// 加入 debug 信息
+	mux.HandleFunc("/debug/pprof", pprof.Index)
+	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+	// 作业中 1 2 3
+	mux.HandleFunc("/", rootHandler)
+	// 4
+	mux.HandleFunc("/healthz", healthzHandler)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
-		glog.V(2).Info(err)
+		log.Fatal("start http server failed, error: %s\n", err.Error())
 	}
 }
 
-func headerHandler(w http.ResponseWriter, req *http.Request) {
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	// header 信息写入
 	header := req.Header
 	for key, values := range header {
 		for _, value := range values {
 			w.Header().Add(key, value)
 		}
 	}
-	w.WriteHeader(200)
-	fmt.Fprintf(w, "getHeader")
-}
 
-func versionHandler(w http.ResponseWriter, req *http.Request) {
+	// 获取 version 配置
+	os.Setenv("VERSION", "v0.0.1")
 	version := os.Getenv("VERSION")
 	w.Header().Add("VERSION", version)
-	fmt.Fprintf(w, "getVersion")
+
+	// 获取 IP 地址
+	clientIp := getIp(req)
+	log.Printf("Success! client ip %s", clientIp)
+
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "SUCCESS")
 }
 
-func welcomeHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "welcome")
+// 获取 IP 地址
+func getIp(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
 }
 
 func healthzHandler(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
-	fmt.Fprintf(w, "200")
+	fmt.Fprintf(w, "running")
 }
